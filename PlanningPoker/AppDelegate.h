1acf121a08fd240f8b34b55834d2dd0eff49de92
crosalot <crosalot@gmail.com>
2012-05-30 14:34:58 +0700

    Initial Commit

---
+++ PlanningPoker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PlanningPoker
+//
+//  Created by Panudate Vasinwattana on 5/30/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlanningPoker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlanningPoker
+//
+//  Created by Panudate Vasinwattana on 5/30/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
